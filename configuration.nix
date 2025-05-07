a879f64e2a101570f4d9a32aaf6bfe1eda1be5c0
emiliano a. <emiliano.anichini@gmail.com>
2025-05-07 18:24:22 +0200

    add 1password

--- configuration.nix
+++ configuration.nix
@@ -23,22 +23,22 @@
 	# 	"1password"
 	# ];}
 
-	# programs = {
-	# 	# NixOS has built-in modules to enable 1Password
-	# 	# along with some pre-packaged configuration to make
-	# 	# it work nicely. You can search what options exist
-	# 	# in NixOS at https://search.nixos.org/options
-	#
-	# 	# Enables the 1Password CLI
-	# 	_1password = { enable = true; };
-	#
-	# 	# Enables the 1Password desktop app
-	# 	_1password-gui = {
-	# 		enable = true;
-	# 		# this makes system auth etc. work properly
-	# 		polkitPolicyOwners = [ "emi" ];
-	# 	};
-	# };
+	programs = {
+		# NixOS has built-in modules to enable 1Password
+		# along with some pre-packaged configuration to make
+		# it work nicely. You can search what options exist
+		# in NixOS at https://search.nixos.org/options
+	
+		# Enables the 1Password CLI
+		_1password = { enable = true; };
+	
+		# Enables the 1Password desktop app
+		_1password-gui = {
+			enable = true;
+			# this makes system auth etc. work properly
+			polkitPolicyOwners = [ "emi" ];
+		};
+	};
 
 	# services.your-service = {
 	#      enable = true;
