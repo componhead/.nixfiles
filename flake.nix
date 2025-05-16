91766be1b049104f2fac9435cd6ac300f779abc6
emiliano a. <emiliano.anichini@gmail.com>
2025-05-16 20:35:42 +0200

    remove nixvim add custom neovim

--- flake.nix
+++ flake.nix
@@ -2,26 +2,43 @@
 	description = "Componhead Flake";
 
 	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-		home-manager.url = "github:nix-community/home-manager/release-24.11";
+    		nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+		home-manager.url = "github:nix-community/home-manager";
 		home-manager.inputs.nixpkgs.follows = "nixpkgs";
-		nixvim = {
-			url = "github:nix-community/nixvim/nixos-24.11";
+		neovim = {
+			url = "github:nix-community/neovim-nightly-overlay";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 	};
 
-	outputs = inputs @ { self, nixpkgs, home-manager, nixvim, ... }:
+	outputs = inputs @ { self, nixpkgs, home-manager, neovim, ... }:
 		let
 			lib = nixpkgs.lib;
 			system = "x86_64-linux";
-			pkgs = nixpkgs.legacyPackages.${system};
+			defaultPackageName = "nvim";
+			overlayFlakeInputs = prev: final: {
+				neovim = neovim.packages.${system}.neovim;
+			};
+			overlayMyNeovim = prev: final: {
+				myNeovim = import ./packages/myNeovim.nix {
+					pkgs = final;
+				};
+			};
+			pkgs = import nixpkgs {
+				overlays = [ overlayFlakeInputs overlayMyNeovim ];
+			};
+
 		in {
 			nixpkgs.config.allowUnfree = true;
+			packages.${system}.default = neovim.packages.${system}.neovim;
+			apps.${system}.default = {
+				type = "app";
+				program = "${neovim.packages.x86_64-linux.neovim}/bin/nvim";
+			};
 			nixosConfigurations = {
 				colossus = let
 					username = "emi";
-					specialArgs = { inherit system; };
+					specialArgs = { inherit neovim; };
 				in
 					lib.nixosSystem {
 						inherit specialArgs;
@@ -31,9 +48,6 @@
             						{
             							home-manager.useGlobalPkgs = true;
             							home-manager.useUserPackages = true;
-								home-manager.sharedModules = [
-									nixvim.homeManagerModules.nixvim
-								];
             							home-manager.extraSpecialArgs = inputs // specialArgs;
             							home-manager.users.${username} = import ./home.nix;
             						}
--- home.nix
+++ home.nix
@@ -2,7 +2,6 @@
 
 {
   # imports = [
-  #   ./kickstart.nixvim/nixvim.nix
   # ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
@@ -98,10 +97,6 @@
 				     }
 			}
 		'';
-	};
-	nixvim = {
-    		enable = true;
-		# more options...
 	};
   }; 
 
